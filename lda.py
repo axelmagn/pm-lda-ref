0356da6ec4f30480210590b50d6e0451084a50d9
Axel Magnuson <axelmagn@gmail.com>
2015-08-28 09:14:00 -0600

    connect cli input with LDA logic

--- lda.py
+++ lda.py
@@ -1,53 +1,78 @@
 """
 Usage:
-    lda.py <DOCUMENTS>
+    lda.py <CORPUS>
 
 Options:
     -k --ntopics    Number of topics to use [default: 20].
     -a --alpha      LDA Alpha Parameter [default: 0.3].
     -b --beta       LDA Beta Parameter [default: 0.3].
+    -f --format     Format of the CORPUS file [default: pickle].
+    -n --iter       Number of sample iterations to perform [default: 2000]
+    -b --burn       Number of samples to discard at first [default: 300]
+    -t --thin       Interval of iterations to measure [default: 1]
 
-A reference implementation of LDA using Gibbs Sampling.  <DOCUMENTS> should be
-a file where each line is a document, represented by a unique identifier
-followed by word counds in the form of WORD:COUNT, separated by spaces.
+A reference implementation of LDA using Gibbs Sampling.
 """
 
-import numpy as np
-import pymc as pm
 from docopt import docopt
+import numpy as np
+import pickle
+import pymc as pm
+import scipy as sp
+from scipy import sparse
 
-class Corpus(object):
-    """
-    A class encapsulating a document corpus
-    """
 
-    def __init__(self, doc_stream):
-        """
-        Construct a new corpus object from a text stream
-        """
-        self.doc_ids = []
-        self.doc_word_counts = []
-        self.vocab = set()
-        for doc_idx, line in enumerate(doc_stream):
-            line = line.strip().split()
-            doc_id = line[0]
-            self.doc_ids.append(doc_id)
+def save_sparse(fname, array):
+    np.savez(fname, data=array.data, indices=array.indices, indptr=array.indptr,
+            shape=array.shape)
 
+def load_csr(fname):
+    loader = np.load(fname)
+    return sparse.csr_matrix((loader['data'], loader['indices'],
+    loader['indptr']), shape=loader['shape'])
+
+def load_csc(fname):
+    loader = np.load(fname)
+    return sparse.csc_matrix((loader['data'], loader['indices'],
+    loader['indptr']), shape=loader['shape'])
+
+def load_pickle(fname):
+    with open(fname, 'rb') as f:
+        return pickle.load(f)
+
+def load(fname, fmt):
+    if fmt == "pickle":
+        return load_pickle(fname)
+    if fmt == "csr":
+        return load_csr(fname)
+    if fmt == "csc":
+        return load_csc(fname)
+    raise ValueError("Unknown file format %s" % fmt)
 
 
 def main(args):
 
-    K = 2  # number of topics
-    V = 4  # number of words
-    D = 3  # number of documents
+    K = int(args['--ntopics'])
+    data = load(args['<CORPUS>'], args['--format'])
+    D = data.shape[0]  # number of documents
+    V = data.shape[1]  # number of words
 
-    data = np.array([[1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 0, 0]])
+    alpha = np.ones(K) * float(args['--alpha'])
+    beta = np.ones(V) * float(args['--alpha'])
 
-    alpha = np.ones(K)
-    beta = np.ones(V)
 
-    theta = pm.Container([pm.CompletedDirichlet("theta_%s" % i, pm.Dirichlet("ptheta_%s" % i, theta=alpha)) for i in range(D)])
-    phi = pm.Container([pm.CompletedDirichlet("phi_%s" % k, pm.Dirichlet("pphi_%s" % k, theta=beta)) for k in range(K)])
+    theta = pm.Container([
+        pm.CompletedDirichlet(
+            "theta_%s" % i,
+            pm.Dirichlet("ptheta_%s" % i, theta=alpha)
+        ) for i in range(D)
+    ])
+    phi = pm.Container([
+        pm.CompletedDirichlet(
+            "phi_%s" % k,
+            pm.Dirichlet("pphi_%s" % k, theta=beta)
+        ) for k in range(K)
+    ])
     Wd = [len(doc) for doc in data]
 
     z = pm.Container([pm.Categorical('z_%i' % d,
@@ -66,8 +91,12 @@ def main(args):
 
     model = pm.Model([theta, phi, z, w])
     mcmc = pm.MCMC(model)
-    mcmc.sample(100)
+    niter = int(args['--iter'])
+    burn = int(args['--burn'])
+    thin = int(args['--thin'])
+    mcmc.sample(niter, burn, thin)
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version="lda.py 0.1.0")
+    print("ARGS: %s" % arguments)
     main(arguments)
